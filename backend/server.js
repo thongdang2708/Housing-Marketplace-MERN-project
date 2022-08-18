4e2bd8a9c893266735b32844a3de7f4893a0b485
thongdang2708 <duythong2708@gmail.com>
2022-08-18 23:55:19 +0300

    changes

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,7 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(__dirname, "../", "frontend", "build", "index.html");
-    });
+    app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"));
 }
 //Server listens
 
